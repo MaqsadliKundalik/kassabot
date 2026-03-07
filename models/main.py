89a4f78adede0cca82e130a6837f64a00772324d
Odilbek <=>
2026-03-07 22:56:06 +0500

    d

--- models/main.py
+++ models/main.py
@@ -13,7 +13,6 @@ class Report(Model):
     user = fields.ForeignKeyField("models.User", related_name="reports")
     caption = fields.TextField()
     price = fields.CharField(max_length=255)
-    created_at = fields.DatetimeField(auto_now_add=True)
     
     class Meta:
         table = "reports"
@@ -23,7 +22,6 @@ class ReportVote(Model):
     report = fields.ForeignKeyField("models.Report", related_name="votes")
     user = fields.ForeignKeyField("models.User", related_name="votes")
     vote = fields.CharField(max_length=255)
-    created_at = fields.DatetimeField(auto_now_add=True)
     
     class Meta:
         table = "report_votes"
