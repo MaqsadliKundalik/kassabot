06d5ec2098482fa33a4b07e320576314b375862f
Odilbek <=>
2026-03-07 22:51:15 +0500

    c

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,6 @@ from tortoise import Tortoise
 async def init_db():
     await Tortoise.init(
         db_url='sqlite://db.sqlite3',
-        modules={'models': ['models.user']}
+        modules={'models': ['models.main']}
     )
     await Tortoise.generate_schemas()
