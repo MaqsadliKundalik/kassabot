338b3eb40de6c3beff29c9d019d7a23d80744c97
Odilbek <=>
2026-03-09 22:50:14 +0500

    d

--- handlers/message/__init__.py
+++ handlers/message/__init__.py
@@ -2,6 +2,6 @@ from . import reports, start, admin
 from aiogram import Router
 
 router = Router()
+router.include_router(admin.router)
 router.include_router(reports.router)
 router.include_router(start.router)
-router.include_router(admin.router)
